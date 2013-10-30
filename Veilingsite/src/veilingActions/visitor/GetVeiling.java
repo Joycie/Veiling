b404ee087b1109bcde32da86a89f5789cbd36b8b
Martijn Schilperoort <me@martijnsch.net>
2013-10-30 12:02:55 +0100

    nog meer veiling detail

--- Veilingsite/src/veilingActions/visitor/GetVeiling.java
+++ Veilingsite/src/veilingActions/visitor/GetVeiling.java
@@ -1,5 +1,18 @@
 package veilingActions.visitor;
+import veilingDomain.Aanbieding;
+import veilingService.VeilingService;
+import com.opensymphony.xwork2.ActionSupport;
 
-public class GetVeiling {
+public class GetVeiling extends ActionSupport {
+	private String id;
+	
+	public String execute() {
+		Aanbieding aanbieding = VeilingService.getAanbieding(id);
+	}
 
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
 }
